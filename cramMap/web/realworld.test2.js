07ceda420f6204f8a7dc75d4a17e062ec2beee63
DouglasWu <dgrey1116@gmail.com>
2017-11-25 15:05:00 +0800

    add processed data and cram school map

---
+++ cramMap/web/realworld.test2.js
@@ -0,0 +1,24 @@
+//An extract of address points from the LINZ bulk extract: http://www.linz.govt.nz/survey-titles/landonline-data/landonline-bde
+//Should be this data set: http://data.linz.govt.nz/#/layer/779-nz-street-address-electoral/
+var addressPoints = [
+      [
+          24.8123912,
+          121.0307527,
+          "愛思可樂教育事業股份有限公司附設新竹縣私立樂研文理短期補習班"
+      ],
+      [
+          24.8150082,
+          121.02083119999998,
+          "丞瑞實業有限公司附設新竹縣私立丞瑞身心適能技藝短期補習班"
+      ],
+      [
+          24.8278985,
+          121.02922209999997,
+          "新竹縣私立芯學淵文理短期補習班"
+      ],
+      [
+          24.8223434,
+          121.01530509999998,
+          "新竹縣私立英王文理短期補習班"
+      ]
+];
